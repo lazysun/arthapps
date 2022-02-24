7a85415c44487e2ec4108d03198f4aa9a67637b4
singhvikas <singhvikas@google.com>
2022-02-23 16:15:37 -0800

    Hello World Commit!

--- src/com/memory/model/pattern/ReleaseBarrier.java
+++ src/com/memory/model/pattern/ReleaseBarrier.java
@@ -3,7 +3,7 @@ package com.memory.model.pattern;
 public class ReleaseBarrier {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello main");
 
 	}
 
